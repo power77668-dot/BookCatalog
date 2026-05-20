6b9360b4b2ef00accb7368f1d7a5102197b96f24
Михаил Никишин <mister_pro228@mail.ru>
2026-05-20 08:41:04 +0300

    Добавлены представления каталога и макет

---
+++ BookCatalog/Views/Books/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookCatalog.Views.Books
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
