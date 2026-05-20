d760fb45896c6595943c90f25e2ddb249a3de891
Михаил Никишин <mister_pro228@mail.ru>
2026-05-20 08:22:10 +0300

    Добавлена модель Book с валидацией

---
+++ BookCatalog/Models/Book.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookCatalog.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Название обязательно")]
+    [StringLength(200, ErrorMessage = "Максимум 200 символов")]
+    [Display(Name = "Название")]
+    public string Title { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Автор обязателен")]
+    [StringLength(100)]
+    [Display(Name = "Автор")]
+    public string Author { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Жанр обязателен")]
+    [StringLength(50)]
+    [Display(Name = "Жанр")]
+    public string Genre { get; set; } = string.Empty;
+
+    [Range(1, 3000, ErrorMessage = "Год должен быть от 1 до 3000")]
+    [Display(Name = "Год издания")]
+    public int Year { get; set; }
+
+    [Range(0, 100000, ErrorMessage = "Цена должна быть положительной")]
+    [Display(Name = "Цена")]
+    public decimal Price { get; set; }
+
+    [StringLength(2000)]
+    [Display(Name = "Описание")]
+    public string? Description { get; set; }
+
+    [Url(ErrorMessage = "Введите корректный URL")]
+    [Display(Name = "URL обложки")]
+    public string? CoverUrl { get; set; }
+}
